8e58a72ef10746e31b05a600ede764c116cf2e0c
simone-garzia <62756636+simone-garzia@users.noreply.github.com>
2021-12-28 18:46:11 +0100

    Pong Game created with LuaLOVE

---
+++ pong-final/Paddle.lua
@@ -0,0 +1,81 @@
+--[[
+    GD50 2018
+    Pong Remake
+
+    -- Paddle Class --
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    Represents a paddle that can move up and down. Used in the main
+    program to deflect the ball back toward the opponent.
+]]
+
+Paddle = Class{}
+
+--[[
+    The `init` function on our class is called just once, when the object
+    is first created. Used to set up all variables in the class and get it
+    ready for use.
+
+    Our Paddle should take an X and a Y, for positioning, as well as a width
+    and height for its dimensions.
+
+    Note that `self` is a reference to *this* object, whichever object is
+    instantiated at the time this function is called. Different objects can
+    have their own x, y, width, and height values, thus serving as containers
+    for data. In this sense, they're very similar to structs in C.
+]]
+function Paddle:init(x, y, width, height)
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.dy = 0
+end
+
+function Paddle:update(dt)
+    -- math.max here ensures that we're the greater of 0 or the player's
+    -- current calculated Y position when pressing up so that we don't
+    -- go into the negatives; the movement calculation is simply our
+    -- previously-defined paddle speed scaled by dt
+    if self.dy < 0 then
+        self.y = math.max(0, self.y + self.dy * dt)
+    -- similar to before, this time we use math.min to ensure we don't
+    -- go any farther than the bottom of the screen minus the paddle's
+    -- height (or else it will go partially below, since position is
+    -- based on its top left corner)
+    else
+        self.y = math.min(VIRTUAL_HEIGHT - self.height, self.y + self.dy * dt)
+    end
+end
+
+--[[
+    Expects a ball as an argument and update paddle , depending
+    on whether the ball is going.
+]]
+
+function Paddle:follow(ball)
+    -- first, check to see if the left edge of either is farther to the right
+    -- than the right edge of the other
+    
+    if ball.y < (self.y + self.height) then
+        self.dy = 3.5*(ball.y - (self.y + self.height))
+    elseif (ball.y + ball.height) > self.y then
+        self.dy = 1.2*((ball.y + ball.height) + self.y)
+    end
+    
+    
+
+end
+
+--[[
+    To be called by our main function in `love.draw`, ideally. Uses
+    LÖVE2D's `rectangle` function, which takes in a draw mode as the first
+    argument as well as the position and dimensions for the rectangle. To
+    change the color, one must call `love.graphics.setColor`. As of the
+    newest version of LÖVE2D, you can even draw rounded rectangles!
+]]
+function Paddle:render()
+    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+end
